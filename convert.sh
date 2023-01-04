c6ff65e63e08e6964abe79b01629c1b9bcb079f4
pintariching <tilen.pintaric99@gmail.com>
2023-01-04 22:16:13 +0100

    add convert script

---
+++ convert.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+for d in */; do 
+    if [ "$d" != "Zapiski/" ]; then
+        mkdir -p Zapiski/$d
+    fi
+done
+
+find ./ -maxdepth 2 -name '*.md'  -type f -exec sh -c 'pandoc ${0} -o Zapiski/${0%.md}.pdf' {} \;
